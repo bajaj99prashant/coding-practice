aa153931c7bd10acbc4c40db3ee6e426c7279c82
bajaj99prashant <prashantbajaj99@gmail.com>
2020-09-20 18:39:15 +0530

    added improved solution for pairWithSomeInSortedAndRotatedArray

--- array/pairWithSumInSortedAndRotatedArray.cpp
+++ array/pairWithSumInSortedAndRotatedArray.cpp
@@ -29,10 +29,65 @@ int* trivialApproach(int x, int arr[], int n){
 
 }
 
+// am not coding but second approach would be to choose a number and apply binary search to the remaining part of the sum.
+// time complexity would be O(nlogn) and space complexity is O(1) but iterative approach can be thought using while loop.
+// but this approach only uses the fact that array is sorted and not rotated.
+
+int findingIndexSmallest(int arr[], int n){
+    int min = arr[0];
+    int j = 0;
+    for (int i = 0; i<n; i++){
+        if (arr[i] < min){
+            j = i;
+            break;
+        }
+    }
+
+    return j;
+}
+
+int* improvedSolution (int x, int arr[], int n){
+    // improved solution is O(n) in time complexity but my solution is O(n) also in space complexity a better implementation 
+    // than this might exist or I should say it exits.
+    int index = findingIndexSmallest(arr, n);
+    int* arr2 = new int[n];
+    int k = n, b =0;
+    while(k > 0){
+        arr2[b] = arr[((b + index) >= n)? (b + index)%n : b + index]; 
+        cout << arr2[b] << ' ';
+        b++;
+        k--;
+    }
+
+    int mid = (n-1)/2;
+    int i = mid;
+    int j = mid+1;
+    int* ar = new int[2];
+
+    while (i >= 0 && j <= n-1){
+        if (arr2[i] + arr2[j] == x){
+            ar[0] = arr2[i];
+            ar[1] = arr2[j];
+            return ar;
+        }
+        else if (arr2[i] + arr2[j] > x){
+            i--;
+        }
+        else if (arr2[i] + arr2[j] < x){
+            j++;
+        }
+    }
+
+    ar[0] = 0;
+    ar[1] = 0;
+
+    return ar;
+}
+
 int main () {
     int arr[] = {11, 15, 6, 8, 9, 10};
     int n = *(&arr+1) - arr;
-    int* ans = trivialApproach(23, arr, n);
+    int* ans = improvedSolution(17, arr, n);
     if (ans[0] == 0 && ans[1] == 0 )
         cout << "not found";
     else
